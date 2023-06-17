79d2a0817d8fac6fefb534dcc0e9584dfd1290a6
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-06-17 17:13:49 -0300

    não funciona

--- lexer.py
+++ lexer.py
@@ -1,99 +1,72 @@
-import re
-
 class Lexer:
     def __init__(self, code):
         self.code = code
         self.position = 0
+        self.current_character = self.code[self.position]
         self.tokens = []
 
-    def tokenize(self):
-        while self.position < len(self.code):
-            if self.code[self.position].isspace():
-                self.position += 1
-            elif self.code[self.position].isdigit():
-                self.number()
-            elif self.code[self.position].isalpha():
-                self.identifier()
-            elif self.code[self.position] == '"':
-                self.string()
-            else:
-                self.special_character()
+    def advance(self):
+        self.position += 1
+        if self.position < len(self.code):
+            self.current_character = self.code[self.position]
+        else:
+            self.current_character = None
 
-        return self.tokens
+    def skip_whitespace(self):
+        while self.current_character is not None and self.current_character.isspace():
+            self.advance()
+
+    def special_character(self):
+        if self.current_character == ';':
+            self.tokens.append(('PONTO_VIRGULA', ';'))
+            self.advance()
+        elif self.current_character == '{':
+            self.tokens.append(('ABRE_CHAVE', '{'))
+            self.advance()
+        elif self.current_character == '}':
+            self.tokens.append(('FECHA_CHAVE', '}'))
+            self.advance()
 
     def number(self):
         number = ''
-        while self.position < len(self.code) and self.code[self.position].isdigit():
-            number += self.code[self.position]
-            self.position += 1
-
-        self.tokens.append(('NUMERO', number))
+        while self.current_character is not None and self.current_character.isdigit():
+            number += self.current_character
+            self.advance()
+        self.tokens.append(('NUMERO', int(number)))
 
     def identifier(self):
         identifier = ''
-        while self.position < len(self.code) and (self.code[self.position].isalpha() or self.code[self.position].isdigit() or self.code[self.position] == '_'):
-            identifier += self.code[self.position]
-            self.position += 1
-
-        if identifier.upper() == 'PROGRAMA':
+        while self.current_character is not None and self.current_character.isalpha():
+            identifier += self.current_character
+            self.advance()
+        if identifier == 'programa':
             self.tokens.append(('PROGRAMA', identifier))
-        elif identifier.upper() == 'FIMPROG':
-            self.tokens.append(('FIM_PROG', identifier))
-        elif identifier.upper() == 'INTEIRO':
+        elif identifier == 'fimprog':
+            self.tokens.append(('FIMPROG', identifier))
+        elif identifier == 'inteiro':
             self.tokens.append(('INTEIRO', identifier))
-        elif identifier.upper() == 'DECIMAL':
-            self.tokens.append(('DECIMAL', identifier))
-        elif identifier.upper() == 'SE':
-            self.tokens.append(('SE', identifier))
-        elif identifier.upper() == 'ENTAO':
-            self.tokens.append(('ENTAO', identifier))
-        elif identifier.upper() == 'SENÃO':
-            self.tokens.append(('SENÃO', identifier))
-        elif identifier.upper() == 'ESCREVA':
-            self.tokens.append(('ESCREVA', identifier))
-        elif identifier.upper() == 'LEIA':
+        elif identifier == 'leia':
             self.tokens.append(('LEIA', identifier))
+        elif identifier == 'escreva':
+            self.tokens.append(('ESCREVA', identifier))
+        elif identifier == 'se':
+            self.tokens.append(('SE', identifier))
+        elif identifier == 'senao':
+            self.tokens.append(('SENAO', identifier))
         else:
             self.tokens.append(('IDENTIFICADOR', identifier))
 
-    def string(self):
-        string = ''
-        self.position += 1  # Ignorar o primeiro caractere (aspas duplas)
-        while self.position < len(self.code) and self.code[self.position] != '"':
-            string += self.code[self.position]
-            self.position += 1
+    def tokenize(self):
+        while self.current_character is not None:
+            if self.current_character.isspace():
+                self.skip_whitespace()
+            elif self.current_character == ';' or self.current_character == '{' or self.current_character == '}':
+                self.special_character()
+            elif self.current_character.isdigit():
+                self.number()
+            elif self.current_character.isalpha():
+                self.identifier()
+            else:
+                raise SyntaxError(f"Token inválido: {self.current_character}")
 
-        if self.position < len(self.code):
-            self.position += 1  # Ignorar o último caractere (aspas duplas)
-
-        self.tokens.append(('STRING', string))
-
-    def special_character(self):
-        if self.code[self.position] == '+':
-            self.tokens.append(('SOMA', '+'))
-        elif self.code[self.position] == '-':
-            self.tokens.append(('SUBTRACAO', '-'))
-        elif self.code[self.position] == '*':
-            self.tokens.append(('MULTIPLICACAO', '*'))
-        elif self.code[self.position] == '/':
-            self.tokens.append(('DIVISAO', '/'))
-        elif self.code[self.position] == ',':
-            self.tokens.append(('VIRGULA', ','))
-        elif self.code[self.position] == '>':
-            self.tokens.append(('MAIOR', '>'))
-        elif self.code[self.position] == '<':
-            self.tokens.append(('MENOR', '<'))
-        elif self.code[self.position] == '=':
-            self.tokens.append(('IGUAL', '='))
-        elif self.code[self.position] == '(':
-            self.tokens.append(('ABRE_PARENTESES', '('))
-        elif self.code[self.position] == ')':
-            self.tokens.append(('FECHA_PARENTESES', ')'))
-        elif self.code[self.position] == '{':
-            self.tokens.append(('ABRE_CHAVE', '{'))
-        elif self.code[self.position] == '}':
-            self.tokens.append(('FECHA_CHAVE', '}'))
-        else:
-            raise SyntaxError(f"Token inválido: {self.code[self.position]}")
-
-        self.position += 1
+        return self.tokens
--- main.py
+++ main.py
@@ -2,34 +2,32 @@ from lexer import Lexer
 from parser_1 import Parser
 from converter import Converter
 
-
 def analyze(code):
     lexer = Lexer(code)
     tokens = lexer.tokenize()
-
     parser = Parser(tokens)
     parser.parse()
-
-    converter = Converter(tokens)
-    converted_code = converter.parse()
-
+    converter = Converter(parser.statements)
+    converted_code = converter.convert()
     return converted_code
 
-
 if __name__ == "__main__":
-    code = """
-    PROGRAMA Teste
+    code = '''
+programa
+inteiro x, y
+{
+    leia(x);
+    leia(y);
+    se (x > y)
     {
-        INTEIRO x, y;
-        x = 5;
-        y = 10;
-        ESCREVA("O valor de x é ", x);
-        ESCREVA("O valor de y é ", y);
-        LEIA(x);
-        ESCREVA("Novo valor de x é ", x);
+        escreva("x é maior que y");
     }
-    FIM_PROG
-    """
-
+    senao
+    {
+        escreva("x não é maior que y");
+    }
+}
+fimprog
+'''
     converted_code = analyze(code)
     print(converted_code)
