62c3d87bdbc9fee853ebaadd26fe92e207b71505
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-06-17 15:24:38 -0300

    ajustes

--- main.py
+++ main.py
@@ -4,23 +4,28 @@ from parser_1 import Parser, Converter
 def analyze(code):
     lexer = Lexer(code)
     tokens = lexer.tokenize()
-
     converter = Converter(tokens)
     converter.parse()
+    python_code = converter.generate_python_code()
+    print(python_code)
 
-
-if __name__ == '__main__':
-    code = """
-    programa
-        inteiro x, y
-        leia(x)
-        leia(y)
-        if (x > y) {
-            escreva("x é maior que y")
-        } else {
-            escreva("x não é maior que y")
-        }
-    fimprog
-    """
-
+if __name__ == "__main__":
+    code = '''
+programa
+inteiro x, y
+{
+    leia(x)
+    leia(y)
+    se x > y
+    {
+        escreva("x é maior que y")
+    }
+    senao
+    {
+        escreva("x não é maior que y")
+    }
+}
+fimprog
+'''
     analyze(code)
+
